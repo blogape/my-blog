e739da79f52fa82b6da88ad036c21791e353bfb1
zengts <905080195@qq.com>
2018-11-26 10:27:07 +0800

    code

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Button } from 'antd';
+import './App.css';
+import 'antd/dist/antd.css'
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      <Button type="primary">Primary</Button>
+      </div>
+    );
+  }
+}
+
+export default App;
